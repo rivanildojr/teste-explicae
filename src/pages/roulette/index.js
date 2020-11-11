10f31fdc00f70ddebc7705f6fc1b8c6408f62a5a
rivanildojr <rivanildo_junior7@hotmail.com>
2020-11-11 17:49:15 -0300

    finalizado a funçao generateArrayRandonAscending - 01

---
+++ src/pages/roulette/index.js
@@ -0,0 +1,27 @@
+const getRandon = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const generateArrayRandon = () => {
+    let sixDozen = new Set();
+
+    while(sixDozen.size < 6) {
+        let number = getRandon(1, 60);
+        sixDozen.add(number);
+    }
+
+    return [...sixDozen];
+}
+
+const sortAscending = (arrayNumber) => {
+    return arrayNumber.sort((a, b) => a - b);
+}
+
+const generateArrayRandonAscending = () => {
+    let arrayNumber = generateArrayRandon();
+    return sortAscending(arrayNumber);
+}
+
+console.log(generateArrayRandonAscending());
